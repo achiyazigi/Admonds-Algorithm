df8ab8ae139b6a11c309083d48de44030f263cda
root <root@LAPTOP-TIC2VA1F.localdomain>
2021-05-28 17:44:36 +0300

    fix

--- src/Admonds_Algo_Util.java
+++ src/Admonds_Algo_Util.java
@@ -1,5 +1,10 @@
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+
 
 public class Admonds_Algo_Util {
 
@@ -158,7 +163,52 @@ public class Admonds_Algo_Util {
     void augment(List<edge_info> path){ // achiya
 
     }
-
+	/**
+	 * @param get src
+	 * the run time of this function is o(v+e)
+	 * sign in the tag of every node the distance from the src 
+	 * and if havn't path to which node her tag will be with -1
+	 * the path kept in the hash map and convert to linked list
+	 */
+	private LinkedList<Integer> bfs(int src,int dest, weighted_graph graph) {
+		if(graph.getV().isEmpty())								//check if it is empty graph
+			return new LinkedList<Integer>();
+	
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();	//keep the nodes in the path -if path exist 
+		map.put(src, 0);	// insert the src node
+		for(node_info keys : graph.getV()) {						//initial the tag's nodes- O(V)
+			keys.setTag(-1);
+		}
+		
+		Queue<Integer> queue=new LinkedList<Integer>();
+		queue.add(graph.getNode(src).getKey());				//keep the key in the queue
+		graph.getNode(src).setTag(0);				//the first node init with 0 										
+		
+		while(!queue.isEmpty()) {
+			Integer loc=queue.poll();
+					for(node_info nei : graph.getV(loc)) {
+						if(nei.getTag() == -1) {
+							queue.add(nei.getKey());
+							nei.setTag(graph.getNode(loc).getTag()+1);			//if have neighbor update the the tag according to his parent
+							map.put(nei.getKey(), loc);
+						}
+					}
+				}
+		
+		//extract the path from the map 
+		LinkedList<Integer> list =new LinkedList<Integer>();
+		if(graph.getNode(dest).getTag() != -1) {	// check if path between the nodes exist
+			int curr = dest;
+			list.add(curr);	//insert the first 
+				while(map.get(curr) != src) {
+					list.add(map.get(curr));
+					curr = map.get(curr); 	//return the neighbord of the current node
+			}
+				list.add(src);
+				Collections.reverse(list);
+				}		
+		return list;
+			}
     /**
      * 
      * @param tree
@@ -167,6 +217,34 @@ public class Admonds_Algo_Util {
      * @return the simple cycle formed in tree including src & dest
      */
     List<Integer> identify_cyc(weighted_graph tree, int src, int dest){ // evyatar
-        return null;
+    	if(src == dest)	//havn't path
+    		return new LinkedList<Integer>();	//return empty linked list 
+    LinkedList<Integer> path = bfs(src, dest,tree);
+    	return path;
     }
+
+public static void main(String args []) {
+	
+	weighted_graph graph = new WGraph_DS();
+	graph.addNode(0);
+	graph.addNode(1);
+	graph.addNode(2);
+	graph.addNode(3);
+	graph.addNode(4);
+	graph.addNode(5);
+
+//	graph.connect(0,1, 0);
+//	graph.connect(1,4, 0);
+//	graph.connect(4,3, 0);
+//	graph.connect(0,2, 0);
+//	graph.connect(2,5, 0);
+	Admonds_Algo_Util adm= new Admonds_Algo_Util(graph);
+	LinkedList<Integer> path = (LinkedList<Integer>) adm.identify_cyc(graph, 1, 1);
+	System.out.println(path);
 }
+
+
+}
+
+
+
